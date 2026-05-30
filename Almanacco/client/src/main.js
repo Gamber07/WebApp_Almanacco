c9f6360574ce58d3b90e1a445bb8b5012e7ff8f8
Gamber07 <gamberi.g@cerebotani.it>
2026-05-31 01:13:50 +0200

    Fix: URL delle API differenziato tra locale e produzione

--- Almanacco/client/src/main.js
+++ Almanacco/client/src/main.js
@@ -15,6 +15,7 @@ import '@fontsource/roboto'
 
 // global axios default (if token in localStorage)
 import axios from 'axios'
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE || (import.meta.env.PROD ? '/api' : '')
 const token = localStorage.getItem('token')
 if (token) axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
 
